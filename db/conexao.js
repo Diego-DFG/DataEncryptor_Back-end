021b131d1a03f1a0c0a4c86d353af392ac11ddf6
Diego Gomes <diegofgomes19@gmail.com>
2022-03-27 20:37:44 -0300

    Alteração na porta do banco

--- db/conexao.js
+++ db/conexao.js
@@ -2,7 +2,6 @@ const mysql = require('mysql2');
 
 const conexao = mysql.createConnection({
     host: process.env.HOST,
-    port: process.env.PORT,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DB
