7ff3553da79267780d23ed94cecf9b064617c86c
Diego Gomes <diegofgomes19@gmail.com>
2022-03-28 20:09:16 -0300

    Alteração na porta

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser');
 
 const url_dev = 'http://127.0.0.1:5500';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser());
